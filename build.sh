3e24ce03376b6c67cfffe121471d3cb02e299d59
Solorad <yarad8613@gmail.com>
2019-05-01 22:10:23 +0200

    Rename build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-platforms=("windows/amd64" "windows/386" "darwin/amd64" "linux/amd64" "linux/arm64")
+set -eux
 
-for platform in "${platforms[@]}"
-do
-    platform_split=(${platform//\// })
-    GOOS=${platform_split[0]}
-    GOARCH=${platform_split[1]}
-    output_name='qor-'$GOOS'-'$GOARCH
-    if [[ $GOOS = "windows" ]]; then
-        output_name+='.exe'
-    fi
+if [ -z ${GOPATH:-} ]; then
+    echo "You need to set \$GOPATH"
+    exit 1
+fi
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name
-    if [[ $? -ne 0 ]]; then
-        echo 'An error has occurred! Aborting the script execution...'
-        exit 1
-    fi
-done
+go get github.com/constabulary/gb/...
+${GOPATH}/bin/gb build all
--- cli-v2.rb
+++ cli-v2.rb
@@ -1,19 +1,17 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CliV2 < Formula
-  desc "Cli v2 (using Go)"
   homepage "https://github.com/Qordobacode/Cli-v2"
-  url "https://github.com/Qordobacode/Cli-v2/releases/tag/version-0.2"
-  sha256 "3246fb72b44e5940dde54c28dc8484d7bd1c7be9d03e0d273d338539ba469dce"
+  url "https://github.com/Qordobacode/Cli-v2/archive/version-0.1.tar.gz"
+  sha256 "8e4bd52a7204526c7ce649bf954c576e0e15c24adcb52e0a1edcdb1204996c79"
+
   depends_on "go" => :build
 
   def install
-     system "gobuild.sh"
-     bin.install ".gobuild/bin/cli-v2" => "qor"
+    ENV["GOPATH"] = buildpath
+    system "build.sh"
+    bin.install ".gobuild/bin/cli-v2" => "qor"
   end
 
   test do
-    system "#{bin}/qor", "--help"
+    system "#{bin}/qor", "--version"
   end
 end

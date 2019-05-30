540dd893499d95c062628a41f3ee86201de28563
Solorad <yarad8613@gmail.com>
2019-05-30 15:50:34 +0300

    Add vendor folder into path

--- build.sh
+++ build.sh
@@ -1,11 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -eux
+platforms=("windows/amd64" "windows/386" "darwin/amd64" "linux/amd64" "linux/arm64")
 
-if [ -z ${GOPATH:-} ]; then
-    echo "You need to set \$GOPATH"
-    exit 1
-fi
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name='qor-'$GOOS'-'$GOARCH
+    if [[ $GOOS = "windows" ]]; then
+        output_name+='.exe'
+    fi
 
-go get github.com/constabulary/gb/...
-${GOPATH}/bin/gb build all
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name
+    if [[ $? -ne 0 ]]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done

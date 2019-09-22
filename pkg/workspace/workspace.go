0ab00b57f0ced23b503a3d2812ddcb3d0f3934bc
Evgenii Morenkov <yarad8613@gmail.com>
2019-09-22 10:44:30 +0200

    Add logic for full workspace load

--- pkg/workspace/workspace.go
+++ pkg/workspace/workspace.go
@@ -5,13 +5,13 @@ import (
 	"github.com/qordobacode/cli-v2/pkg"
 	"github.com/qordobacode/cli-v2/pkg/general/log"
 	"github.com/qordobacode/cli-v2/pkg/types"
-	"strconv"
 	"time"
 )
 
 const (
-	getWorkspacesTemnplate = "%s/v3/organizations/%d/workspaces"
-	workspaceFileName      = "workspace.json"
+	getWorkspacesTemplate = "%s/v3/organizations/%d/workspaces?limit=%d&offset=%d"
+	limit                 = 500
+	workspaceFileName     = "workspace.json"
 )
 
 // Service implements pkg.Service
@@ -23,20 +23,20 @@ type Service struct {
 
 // LoadWorkspace function retrieves a workspace
 func (w *Service) LoadWorkspace() (*types.WorkspaceData, error) {
-	start := time.Now()
-	defer func() {
-		log.TimeTrack(start, "LoadWorkspace "+strconv.Itoa(int(w.Config.Qordoba.WorkspaceID)))
-	}()
 	workspaceResponse, err := w.cachedWorkspace()
-	if err != nil || workspaceResponse == nil {
-		workspaceResponse, err = w.workspacesFromServer()
+	if err == nil && workspaceResponse != nil {
+		for _, workspaceData := range workspaceResponse.Workspaces {
+			if workspaceData.Workspace.ID == int(w.Config.Qordoba.WorkspaceID) {
+				return &workspaceData, nil
+			}
+		}
 	}
-	if err != nil {
-		return nil, err
-	}
-	for _, workspaceData := range workspaceResponse.Workspaces {
-		if workspaceData.Workspace.ID == int(w.Config.Qordoba.WorkspaceID) {
-			return &workspaceData, nil
+	workspaceResponse, err = w.workspacesFromServer()
+	if err == nil && workspaceResponse != nil {
+		for _, workspaceData := range workspaceResponse.Workspaces {
+			if workspaceData.Workspace.ID == int(w.Config.Qordoba.WorkspaceID) {
+				return &workspaceData, nil
+			}
 		}
 	}
 	err = fmt.Errorf("workspace with id=%v was not found", string(w.Config.Qordoba.WorkspaceID))
@@ -62,19 +62,50 @@ func (w *Service) cachedWorkspace() (*types.WorkspaceResponse, error) {
 
 // workspacesFromServer function retrieve list of all workspaces
 func (w *Service) workspacesFromServer() (*types.WorkspaceResponse, error) {
+	log.Infof("start to download %v organization's workspace structure...", w.Config.Qordoba.WorkspaceID)
+	start := time.Now()
 	base := w.Config.GetAPIBase()
-	// retrieve from server list of workspaces
-	workspaceRequestURL := fmt.Sprintf(getWorkspacesTemnplate, base, w.Config.Qordoba.OrganizationID)
-	bodyBytes, err := w.QordobaClient.GetFromServer(workspaceRequestURL)
-	if err != nil {
-		return nil, err
+	result := &types.WorkspaceResponse{
+		Meta: types.Meta{
+			Paging: types.Paging{
+				TotalEnabled: 0,
+				TotalResults: 1,
+			},
+		},
+		Workspaces: make([]types.WorkspaceData, 0),
 	}
-	var workspaceResponse types.WorkspaceResponse
-	err = workspaceResponse.UnmarshalJSON(bodyBytes)
-	if err != nil {
-		log.Errorf("error occurred on request for workspace: %v", err)
-		return nil, err
+
+	errNum := 0
+	for offset := 0; offset < result.Meta.Paging.TotalResults; offset += limit {
+		// retrieve from server list of workspaces
+		workspaceRequestURL := fmt.Sprintf(getWorkspacesTemplate, base, w.Config.Qordoba.OrganizationID, limit, offset)
+		bodyBytes, err := w.QordobaClient.GetFromServer(workspaceRequestURL)
+		if err != nil {
+			if errNum == 0 {
+				// try to repeat failed request 1 time
+				offset -= limit
+				errNum++
+			}
+			continue
+		}
+		var workspaceResponse types.WorkspaceResponse
+		err = workspaceResponse.UnmarshalJSON(bodyBytes)
+		if err != nil {
+			log.Errorf("error occurred on request for workspace: %v", err)
+			continue
+		}
+		result.Meta.Paging.TotalResults = workspaceResponse.Meta.Paging.TotalResults
+		result.Meta.Paging.TotalEnabled = workspaceResponse.Meta.Paging.TotalEnabled
+		result.Workspaces = append(result.Workspaces, workspaceResponse.Workspaces...)
+		errNum = 0
+		log.Infof("request %d/%d...", offset/limit+1, (result.Meta.Paging.TotalResults+limit-1)/limit)
 	}
-	w.Local.PutInHome(workspaceFileName, bodyBytes)
-	return &workspaceResponse, nil
+
+	bytes, err := result.MarshalJSON()
+	if err == nil {
+		w.Local.PutInHome(workspaceFileName, bytes)
+	}
+	elapsed := time.Since(start)
+	log.Infof("Organization %d workspace structure is downloaded in %v. %d workspaces were downloaded", w.Config.Qordoba.WorkspaceID, elapsed, len(result.Workspaces))
+	return result, nil
 }
